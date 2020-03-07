a021315b3eb34a9be926fa4e25810319757019b8
budizhao <budizhao@gmail.com>
2020-03-07 19:04:37 +0000

    First version fulfil most functions

---
+++ SHPSG.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+def SHPSG(Ei, Fi, D2_8, D9_15):
+    #-------------------------------------------------------------------------------------------------------
+    # SHPSG.V1: Spherical Harmonic Particle Shape Generator (Feb 2018)
+    #   -By Deheng WEI, Budi Zhao and Jeff Wang
+    #   -Please refer to Wei et al. (2018) and Zhao et al. (2017)
+    #-------------------------------------------------------------------------------------------------------
+    Fvec = np.zeros((4,3),dtype=complex)
+    # Determine C0 and C1 with Ei, Fi and a unit maximum principal dimension 
+    # A sphere with unit diameter  
+    fvec_sphere = -np.sqrt(np.pi/6)*np.array([[0,0,0],[-1,1j,0],[0,0,np.sqrt(2)],[1,1j,0]]) 
+    Fvec[0:4,0] = fvec_sphere[:,0]
+    Fvec[0:4,1] = Ei*fvec_sphere[:,1]
+    Fvec[0:4,2] = (Fi*Ei)*fvec_sphere[:,2]
+    d1 = np.sqrt(sum(sum((Fvec*np.conj(Fvec))))).real
+
+    # Determine C2-C15 with d2_8 and d9_16
+    # Determine d2 and d9
+    #   Assume alpha = 1.387 and beta  = 1.426
+    D_2 = D2_8/((2/2)**1.387+(2/3)**1.387+(2/4)**1.387+(2/5)**1.387+(2/6)**1.387+(2/7)**1.387+(2/8)**1.387)*d1
+    D_9 = D9_15/((9/9)**1.426+(9/10)**1.426+(9/11)**1.426+(9/12)**1.426+(9/13)**1.426+(9/14)**1.426+(9/15)**1.426)*d1
+
+    # Determine d3-d8 and d10-d15 
+    #   -Assume all descriptors have three identical decomposition at x-, y- and z-axis
+    I = np.zeros((16,3),dtype = float)
+    I[1,:] = [1,1,1]
+    I[2,:] = [D_2,D_2,D_2]
+    for c in range(3,9):
+        dn = [D_2*((c-1)/2)**(-1.387)/np.sqrt(3),D_2*((c-1)/2)**(-1.387)/np.sqrt(3),
+              D_2*((c-1)/2)**(-1.387)/np.sqrt(3)]
+        I[c,:] = dn
+
+    for c in range(9,15):
+        dn = [D_9*((c-1)/9)**(-1.426)/np.sqrt(3),D_9*((c-1)/9)**(-1.426)/np.sqrt(3),
+              D_9*((c-1)/9)**(-1.426)/np.sqrt(3)]
+        I[c,:] = dn
+
+    L = np.zeros((16**2,3))
+    N = np.zeros((16**2,3))
+
+    # Randomly generate P including C1'-C15' with c_n^(-m)=(-1)^m*c_n^m*
+    for n in range(1,16):
+        J = np.ones((n+1,3))-2*np.random.rand(n+1,3) # [-1,1]
+        K = np.flipud(J[0:n,:])
+        A = [[(-1)**n,(-1)**n,(-1)**n]]
+        B = K*A
+        L[n**2:(n+1)**2,:] = np.append(J,B,axis=0)
+        M = np.ones((n,3))-2*np.random.rand(n,3)
+        M1 = np.append(M,[[0,0,0]],axis = 0)
+        N[n**2:(n+1)**2,:] = np.append(M1,np.flipud((-1)**(n+1)*M),axis = 0)
+
+    P = L+N*1j
+    P[0,:] = np.ones((1,3))-2*np.random.rand(1,3)
+
+    # Calculate d1'-d16' with the SH coeffiecients of P
+    Q = np.conj(P)
+    R = np.zeros((16,3))
+
+    for n in range(16):
+        R[n,:] = np.sqrt(sum(P[n**2:(n+1)**2,:]*Q[n**2:(n+1)**2,:]).real)
+
+    # Determine C2-C15 by making the descriptors of P equal to d2-d15
+    fvec = np.zeros((16**2,3),dtype = complex)
+    fvec[0:4,:] = Fvec
+    for d in range(2,16):
+        fvec[d**2:(d+1)**2,0] = P[d**2:(d+1)**2,0]/R[d,0]*I[d,0]
+        fvec[d**2:(d+1)**2,1] = P[d**2:(d+1)**2,1]/R[d,1]*I[d,1]
+        fvec[d**2:(d+1)**2,2] = P[d**2:(d+1)**2,2]/R[d,2]*I[d,2]
+    return fvec
---
+++ funcs.py
@@ -0,0 +1,112 @@
+import numpy as np
+from scipy.special import sph_harm
+
+# calcualte coordinates with SH expansion
+def sph2cart(coeff, phi, theta):
+    x = 0
+    y = 0
+    z = 0
+    index = 0
+    for n in range(9):
+        order = [*range(-n,n+1)]
+        for m in range(2*n+1):
+            x += coeff[index,0]*sph_harm(order[m], n, theta, phi)
+            y += coeff[index,1]*sph_harm(order[m], n, theta, phi)
+            z += coeff[index,2]*sph_harm(order[m], n, theta, phi)
+            index += 1
+    return x, y, z
+
+# define icosahedron surface
+def icosahedron():
+    # create unit regular icosahedron
+    t = (1+np.sqrt(5)) / 2
+    #create vertices
+    v = np.array([[-1, t, 0], # v1
+         [1, t, 0], # v2
+         [-1,-t, 0], # v3
+         [1,-t, 0], # v4
+         [0,-1, t], # v5
+         [0, 1, t], # v6
+         [0,-1,-t], # v7
+         [0, 1,-t], # v8
+         [t, 0,-1], # v9
+         [t, 0, 1], # v10
+         [-t, 0,-1], # v11
+         [-t, 0, 1]])# v12
+    # normalise vertices to unit size
+    v = v/np.linalg.norm(v[0,:])/2
+    #create faces
+    f = np.array([[0,11, 5], # f1
+         [0, 5, 1], # f2
+         [0, 1, 7], # f3
+         [0, 7,10], # f4
+         [0,10,11], # f5
+         [1, 5,9], # f6
+         [5,11, 4], # f7
+         [11,10, 2], # f8
+         [10, 7, 6], # f9
+         [7, 1, 8], # f10
+         [3,9, 4], # f11
+         [3, 4, 2], # f12
+         [3, 2, 6], # f13
+         [3, 6, 8], # f14
+         [3, 8,9], # f15
+         [4,9, 5], # f16
+         [2, 4,11], # f17
+         [6, 2,10], # f18
+         [8, 6, 7], # f19
+         [9, 8, 1]])# f20
+    return v,f
+
+# calculate sphercial coordinates based on xyz
+def car2sph(xyz):
+    ptsnew = np.hstack((xyz, np.zeros(xyz.shape)))
+    xy = xyz[:,0]**2 + xyz[:,1]**2
+    ptsnew[:,3] = np.sqrt(xy + xyz[:,2]**2)
+    ptsnew[:,4] = np.arctan2(np.sqrt(xy), xyz[:,2]) # for elevation angle defined from Z-axis down
+    #ptsnew[:,4] = np.arctan2(xyz[:,2], np.sqrt(xy)) # for elevation angle defined from XY-plane up
+    ptsnew[:,5] = np.arctan2(xyz[:,1], xyz[:,0])
+    return ptsnew
+
+# subdivide triangle faces
+def subdivsurf(f,v):
+    f_ = np.zeros((len(f)*4,3))
+    for i in range(len(f)): # for each triangle
+        tri = f[i,:]
+        # calculate mid points (add new points to v)
+        [a,v] = getMidPoint(tri[0],tri[1],v)
+        [b,v] = getMidPoint(tri[1],tri[2],v)
+        [c,v] = getMidPoint(tri[2],tri[0],v)
+        # generate new subdivision triangles
+        nfc = np.array([[tri[0],a,c],
+               [tri[1],b,a],
+               [tri[2],c,b],
+               [a,b,c]])
+        # replace triangle with subdivision
+        idx = range(4*i,4*(i+1),1)
+        f_[idx,:] = nfc
+    return v,f_.astype(int)
+
+def getMidPoint(t1,t2,v):
+    # GETMIDPOINT calculates point between two vertices
+    # Calculate new vertex in sub-division and normalise to unit length
+    # then find or add it to v and return index
+    # get vertice positions
+    p1 = v[t1,:]
+    p2 = v[t2,:]
+    # calculate mid point (on unit sphere)
+    pm = (p1 + p2) / 2
+    pm = pm/np.linalg.norm(pm)/2
+    # add to vertices list, return index
+    i = len(v)
+    v = np.vstack((v,pm))
+    return [i,v]
+
+def cleanmesh(f,v):
+    # remove duplicate vertices
+    v,AC,TC = np.unique(v,return_index = True, return_inverse=True,axis = 0)
+    # reassign faces to trimmed vertex list
+    for i in range(len(f)):
+        for j in range(3):
+            f[i,j] = TC[f[i,j]]
+    return v,f
